5782a1e4b74e29b18c1a0d4a89701dc9323ecc4f
chenchen151 <chenchen30@jd.com>
2017-11-20 21:56:19 +0800

    1

---
+++ src/main/java/com/jason/sort/Search.java
@@ -0,0 +1,41 @@
+package com.jason.sort;
+
+import org.junit.jupiter.api.Test;
+
+public class Search {
+
+    /**
+     * 二分法查找的前提是：数组必须是有序的
+     */
+
+    //循环
+    public int getPos(int[] a,int val){
+        int l = 0;
+        int h = a.length;
+        while(l<=h){
+            int mid = (l+h)/2;
+            if(a[mid]>val)
+                h = mid;
+            if(a[mid] <val)
+                l = mid;
+            return mid;
+        }
+        return -1;
+    }
+    //递归
+    public int getPos(int[] a,int val,int l,int h){
+        int mid = (l+h)/2;
+        if(a[mid]>val)
+            getPos(a, val, l, mid);
+        if(a[mid]<val)
+            getPos(a, val,mid,h);
+        return mid;
+    }
+
+    @Test
+    public void test(){
+        int[] a = new int[]{1,5,6,7,8,10};
+        int index = getPos(a,7);
+        System.out.println(index);
+    }
+}
