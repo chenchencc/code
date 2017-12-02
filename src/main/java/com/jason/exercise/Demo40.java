a0e1e87c929fa5c9eb59be8ea7cb7320c6f118d1
chenchen151 <chenchen30@jd.com>
2017-12-02 19:56:09 +0800

    1

--- src/main/java/com/jason/exercise/Demo40.java
+++ src/main/java/com/jason/exercise/Demo40.java
@@ -1,11 +1,27 @@
 package com.jason.exercise;
 
+import com.jason.common.ListNode;
+
+import java.util.Stack;
+
 public class Demo40 {
 
     /**
      * 输入个链表的头结点，从尾到头反过来打印出每个结点的值
      */
-    public void reverse(){
+    public void reverse(ListNode node){
+        Stack<ListNode> stack = new Stack<>();
+        if (node!=null){
+            ListNode root = node;
+            while (root!=null){
+                stack.push(root);
+                root = root.next;
+            }
+        }
+
+        while (!stack.isEmpty()){
+            System.out.println(stack.pop());
+        }
 
     }
 }
