ec5f0298dc8521ce716539a26cb4fc6857a53935
chenchen151 <chenchen30@jd.com>
2018-02-01 09:52:10 +0800

    1

---
+++ src/main/java/com/jason/nio/FileDemo.java
@@ -0,0 +1,14 @@
+package com.jason.nio;
+
+import org.junit.Test;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class FileDemo {
+
+    @Test
+    public void test(){
+
+    }
+}
